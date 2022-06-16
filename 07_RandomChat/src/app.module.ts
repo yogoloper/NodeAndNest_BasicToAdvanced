671ceb8b0ae58ffbc5bfea288ebaae6f8e51c858
yogoloper <jguddnjs@gmail.com>
2022-06-17 00:30:57 +0900

    chapter 07 chat 게이트웨이, 클라이언트 작성

--- 07_RandomChat/src/app.module.ts
+++ 07_RandomChat/src/app.module.ts
@@ -2,19 +2,23 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ChatsGateway } from './chats/chats.gateway';
+import { ChatsModule } from './chats/chats.module';
 import mongoose from 'mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }),
+    // MongooseModule.forRoot(process.env.MONGODB_URI, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    // }),
+    ChatsModule,
   ],
   controllers: [AppController],
+  providers: [ChatsGateway],
 })
 export class AppModule implements NestModule {
   private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
